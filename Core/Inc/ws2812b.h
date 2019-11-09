a2cc6f899f2fc457cf11c717b2d0a5baabe176e3
Sam Kent <sam.john.kent@gmail.com>
2019-11-09 14:54:37 +0100

    w2182b timings

--- Core/Inc/ws2812b.h
+++ Core/Inc/ws2812b.h
@@ -3,8 +3,8 @@
  */
 #include "main.h"
 
-#define WS2812B_LOW 2
-#define WS2812B_HIGH 14
+#define WS2812B_LOW  5 
+#define WS2812B_HIGH 10
 
 void ws2812b_init();
 void ws2812b_set_pixel(uint8_t n, uint32_t grb, uint32_t mask);
